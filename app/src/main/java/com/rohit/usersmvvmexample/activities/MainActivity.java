e4c6fd936a65d94d1b42e4ed1be16e5179b9615f
Rohit Gupta <er.rohitgupta@outlook.com>
2017-07-26 01:12:26 +0530

    added layout files and resource files

--- app/src/main/java/com/rohit/usersmvvmexample/activities/MainActivity.java
+++ app/src/main/java/com/rohit/usersmvvmexample/activities/MainActivity.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.rohit.usersmvvmexample.UsersMVVMApplication;
+
+import javax.inject.Inject;
+
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    Realm realm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((UsersMVVMApplication) getApplication()).getObjectsComponent().inject(this);
     }
 
 }
--- app/src/main/java/com/rohit/usersmvvmexample/dagger/component/ObjectsComponent.java
+++ app/src/main/java/com/rohit/usersmvvmexample/dagger/component/ObjectsComponent.java
@@ -1,5 +1,7 @@
 package com.rohit.usersmvvmexample.dagger.component;
 
+import com.rohit.usersmvvmexample.activities.MainActivity;
+import com.rohit.usersmvvmexample.dagger.module.AppModule;
 import com.rohit.usersmvvmexample.dagger.module.ObjectsModule;
 
 import javax.inject.Singleton;
@@ -7,6 +9,9 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = ObjectsModule.class)
+@Component(modules = {AppModule.class, ObjectsModule.class})
 public interface ObjectsComponent {
+
+    void inject(MainActivity mainActivity);
+
 }
